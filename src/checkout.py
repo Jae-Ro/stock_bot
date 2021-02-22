2ea9879da5a156556d42dbc5992cca0ab41212ca
jae-ro <jaegro93@gmail.com>
2021-02-21 22:22:34 -0500

    reverting to previous version of parsing sitename from config

--- src/checkout.py
+++ src/checkout.py
@@ -10,16 +10,15 @@ def main(args):
     logging = logger.create_logger()
     if args['bestbuy']:
         config_file_path = "./configs/bestbuy_config.json"
-        site_name = "bestbuy"
     elif args['walmart']:
         config_file_path="./configs/walmart_config.json"
-        site_name="walmart"
     else:
         logging.info("No website was selected. Terminating Program.")
         return
     with open(config_file_path) as file:
         config = json.load(file)
     products = config['products']
+    site_name = config_file_path.split("/")[-1].split("_config.json")[0]
     bot = StockBot(site=site_name, username=env_config(f"{site_name.upper()}_USERNAME"), password=env_config(f"{site_name.upper()}_PASSWORD"), 
                     website_dict=config['website'], product_dict=products[0], 
                     logger=logging, cvv_code=env_config("CVV"), max_price=env_config("MAX_PRICE"), 
