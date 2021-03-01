b04b7a7efd8991e5dfc1a898abf09720ec6e16be
jae-ro <jaegro93@gmail.com>
2021-02-28 19:58:41 -0500

    updating logger to show name of store and what you're looking for

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,20 +1,29 @@
 import logging
 import os
 
-def create_logger(dt_str):
+
+class Filter(logging.Filter):
+    def __init__(self, name):
+        self.name = name
+    def filter(self, record):
+        record.app_name = self.name
+        return True
+
+def create_logger(dt_str, logger_name):
     # Initital Logger Setup
     logger = logging.getLogger("stock_bot")
+    logger.addFilter(Filter(logger_name))
     logger.setLevel(logging.DEBUG)
 
     # File Logging
     file_log_handler = logging.FileHandler(f"../logs/stock_bot_{dt_str}.log")
-    file_log_handler.setFormatter(logging.Formatter("%(asctime)s %(filename)s [%(levelname)4s] %(message)s"))
+    file_log_handler.setFormatter(logging.Formatter("%(asctime)s %(filename)s [%(app_name)4s] %(message)s"))
     file_log_handler.setLevel(logging.DEBUG)
 
     # Stream Logging to StdOut
     LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter("%(asctime)s %(filename)s [%(levelname)4s] %(message)s"))
+    stream_handler.setFormatter(logging.Formatter("%(asctime)s %(filename)s [%(app_name)4s] %(message)s"))
     stream_handler.setLevel(LOGLEVEL)
 
     # Combine Stream and File Logging into same Logger
